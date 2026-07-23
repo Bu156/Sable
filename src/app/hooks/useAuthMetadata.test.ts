6627b92f0a7c3007638f4cdfb970b093d1717440
7w1 <noreply@sl.sable.moe>
2026-07-23 14:12:57 -0500

    Update useAuthMetadata.test.ts

--- src/app/hooks/useAuthMetadata.test.ts
+++ src/app/hooks/useAuthMetadata.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest';
 import type { ValidatedAuthMetadata } from '$types/matrix-sdk';
 import type { Session } from '$state/sessions';
 import { getSessionAuthMetadata } from './useAuthMetadata';
