260a00e9ecfdcdba5630445111c28a97b5bbe235
Erwan Leboucher <erwanleboucher@gmail.com>
2026-08-07 11:51:15 +0200

    fix: resolve clippy errors from cargo dependency bumps

--- src-tauri/src/network/native_upload.rs
+++ src-tauri/src/network/native_upload.rs
@@ -7,12 +7,12 @@ use std::{
 
 use base64::Engine;
 use futures_util::StreamExt;
-use serde::Serialize;
-use tauri::{ipc::Channel, AppHandle, Manager, Runtime};
-use tauri_plugin_http::reqwest::{
+use reqwest::{
     header::{AUTHORIZATION, CONTENT_LENGTH, CONTENT_TYPE},
     Body, Client, ClientBuilder, Method, Url,
 };
+use serde::Serialize;
+use tauri::{ipc::Channel, AppHandle, Manager, Runtime};
 use tokio::{
     fs::{File, OpenOptions},
     io::AsyncWriteExt,
