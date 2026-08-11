ab459297b92ed3f83c3e1cc313a11c3de354ce0e
Erwan Leboucher <erwanleboucher@gmail.com>
2026-08-11 23:30:58 +0200

    feat: package Flatpak with extra data

--- packaging/flatpak/sable.sh
+++ packaging/flatpak/sable.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # zypak lets CEF's sandbox work under Flatpak without a setuid chrome-sandbox.
-exec zypak-wrapper /app/sable/sable "$@"
+export ZYPAK_CEF_LIBRARY_PATH=/app/extra/sable/libcef.so
+exec zypak-wrapper /app/extra/sable/sable "$@"
